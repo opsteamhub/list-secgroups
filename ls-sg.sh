02b7c20fd8687eb525681b5de76db5775ee5291a
Robson Franco <contato@robsonfrancos.com.br>
2022-06-15 17:57:43 -0300

    add bash script

---
+++ ls-sg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ports=(22 3389 3306 5432 27017)
+regions=("us-east-1" "sa-east-1")
+myaccount=$(aws sts get-caller-identity --query Account --output text)
+
+echo -e "Account ID: $myaccount\n"
+
+ri=0
+rlen=${#regions[@]}
+while [ $ri -lt $rlen ]
+do
+    echo -e '\nRegion: '${regions[$ri]}
+    pi=0
+    plen=${#ports[@]}
+    while [ $pi -lt $plen ]
+    do
+        echo 'Port: '${ports[$pi]}
+        aws ec2 describe-security-groups \
+            --filters Name=ip-permission.from-port,Values=${ports[$pi]} Name=ip-permission.to-port,Values=${ports[$pi]} Name=ip-permission.cidr,Values='0.0.0.0/0' \
+            --query 'SecurityGroups[?IpPermissions[?ToPort==`'${ports[$pi]}'` && contains(IpRanges[].CidrIp, `0.0.0.0/0`)]].{GroupId: GroupId, GroupName: GroupName}' \
+            --output text \
+            --region ${regions[$ri]};
+        let pi++
+        echo -e "\n"
+    done
+    echo "============================="
+    let ri++
+done
